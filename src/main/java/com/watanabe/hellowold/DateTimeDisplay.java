e78fc20e233266959841b191b9fef5e752a98420
watanabemasao <104755064+watanabemasao@users.noreply.github.com>
2022-06-13 23:00:42 +0900

    DateTimeZoneクラスを削除、不要なimplementationの削除

--- src/main/java/com/watanabe/hellowold/DateTimeDisplay.java
+++ src/main/java/com/watanabe/hellowold/DateTimeDisplay.java
@@ -5,22 +5,27 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeDisplay {
-    DateTimeZone dateTimeZone = new DateTimeZone();
+
+    private final String DATA_TIME = "yyyy年MM月dd日(E)HH時mm分ss秒";
+    private final String JPN = "Asia/Tokyo";
+    private final String CST = "Asia/Shanghai";
+    private final String USP = "US/Pacific";
+
     public String dateTimeJapan() {
-        OffsetDateTime offsetJapan = OffsetDateTime.now(ZoneId.of(dateTimeZone.getJPN()));
-        var formatterJapan = DateTimeFormatter.ofPattern(dateTimeZone.getDATA_TIME());
+        OffsetDateTime offsetJapan = OffsetDateTime.now(ZoneId.of(JPN));
+        var formatterJapan = DateTimeFormatter.ofPattern(DATA_TIME);
         return offsetJapan.format(formatterJapan);
     }
 
     public String dateTimeShanghai() {
-        OffsetDateTime offsetShanghai = OffsetDateTime.now(ZoneId.of(dateTimeZone.getCST()));
-        var formatterShanghai = DateTimeFormatter.ofPattern(dateTimeZone.getDATA_TIME());
+        OffsetDateTime offsetShanghai = OffsetDateTime.now(ZoneId.of(CST));
+        var formatterShanghai = DateTimeFormatter.ofPattern(DATA_TIME);
         return offsetShanghai.format(formatterShanghai);
     }
 
     public String dateTimeUS_Pacific() {
-        OffsetDateTime offsetUS_Pacific = OffsetDateTime.now(ZoneId.of(dateTimeZone.getUSP()));
-        var formatterUS_Pacific = DateTimeFormatter.ofPattern(dateTimeZone.getDATA_TIME());
+        OffsetDateTime offsetUS_Pacific = OffsetDateTime.now(ZoneId.of(USP));
+        var formatterUS_Pacific = DateTimeFormatter.ofPattern(DATA_TIME);
         return offsetUS_Pacific.format(formatterUS_Pacific);
     }
 
