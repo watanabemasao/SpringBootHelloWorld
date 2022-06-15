41139724de7dddf8731961f15afa42cf477ec448
watanabemasao <104755064+watanabemasao@users.noreply.github.com>
2022-06-16 02:07:57 +0900

    enumの引数の値をprivateにし、getterを配置、HelloControllerクラスにHelloDefinitionをインポート

--- src/main/java/com/watanabe/hellowold/HelloController.java
+++ src/main/java/com/watanabe/hellowold/HelloController.java
@@ -1,25 +1,22 @@
 package com.watanabe.hellowold;
 
 import com.watanabe.hellowold.domain.hello.HelloEntity;
-import com.watanabe.hellowold.domain.hello.HelloDefinition;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+import static com.watanabe.hellowold.domain.hello.HelloDefinition.*;
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String helloList(Model model) {
         DateTimeDisplay dateTimeDisplay = new DateTimeDisplay();
         var helloList = List.of(
-                new HelloEntity(HelloDefinition.JP, HelloDefinition.JP.country, HelloDefinition.JP.capital,
-                        HelloDefinition.JP.greeting, dateTimeDisplay.dateTimeJapan()),
-                new HelloEntity(HelloDefinition.CH, HelloDefinition.CH.country, HelloDefinition.CH.capital,
-                        HelloDefinition.CH.greeting, dateTimeDisplay.dateTimeShanghai()),
-                new HelloEntity(HelloDefinition.US, HelloDefinition.US.country, HelloDefinition.US.capital,
-                        HelloDefinition.US.greeting, dateTimeDisplay.dateTimeUS_Pacific())
+                new HelloEntity(JP, JP.getCapital(), JP.getCountry(), JP.getGreeting(), dateTimeDisplay.dateTimeJapan()),
+                new HelloEntity(CH, CH.getCapital(), CH.getCountry(), CH.getGreeting(), dateTimeDisplay.dateTimeShanghai()),
+                new HelloEntity(US, US.getCapital(), US.getCountry(), US.getGreeting(), dateTimeDisplay.dateTimeUS_Pacific())
         );
         model.addAttribute("helloList", helloList);
         return "hello";
--- src/main/java/com/watanabe/hellowold/domain/hello/HelloDefinition.java
+++ src/main/java/com/watanabe/hellowold/domain/hello/HelloDefinition.java
@@ -1,17 +1,27 @@
 package com.watanabe.hellowold.domain.hello;
 
 public enum HelloDefinition {
-    JP("日本", "東京","こんにちは！"),
-    CH("中国", "北京","你好！"),
-    US("アメリカ","ワシントンD.C.","Hello!");
+    JP("日本", "東京", "こんにちは！"),
+    CH("中国", "北京", "你好！"),
+    US("アメリカ", "ワシントンD.C.", "Hello!");
 
-    public final String country;
-    public final String capital;
-    public final String greeting;
-
-    HelloDefinition(String country, String capital, String greeting) {
+    private final String country;
+    private final String capital;
+    private final String greeting;
+    HelloDefinition(final String country, final String capital, final String greeting) {
         this.country = country;
         this.capital = capital;
         this.greeting = greeting;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getGreeting() {
+        return greeting;
     }
 }
